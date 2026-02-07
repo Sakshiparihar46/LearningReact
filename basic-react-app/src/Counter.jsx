acda4c2b792da7f76ce693e59374c81a9e655757
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-02-08 00:43:37 +0530

    learnn about use case

--- basic-react-app/src/Counter.jsx
+++ basic-react-app/src/Counter.jsx
@@ -1,18 +1,22 @@
-import { useState } from "react"
+import { useState,useEffect} from "react"
+export default function(){
+let [countX,setCountX]=useState(0);
+let [countY,setCountY]=useState(0);
+     let inCountX=()=>{
+        setCountX((currCount)=> currCount+1);
+     }
+     let inCountY=()=>{
+        setCountY((currCount)=> currCount+1);
+     }
 
-export default function Counter(){
-    let [count,setCount]=useState(0);
-    console.log("Component is rendered : ");
-    console.log(`${count}`);
-    let increaseCount=()=>{
-        setCount(count+1);
-        console.log(`inside increase count is ${count}`)
-    }
-     return (
-        <>
-        <h3>count={count}</h3>
-        <button onClick={increaseCount}>increase count</button>
-        </>
-     )
-}
+     useEffect(function printSomething(){
+        console.log("this is a side effect");
+     },[])
+    return <div>
+        <h3>countX={countX}</h3>
+        <button onClick={inCountX}>+1</button>
+        <h3>countY={countY}</h3>
+        <button onClick={inCountY}>+1</button>
+    </div>
+} 
 
